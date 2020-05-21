602131dee163528d10be58af6dddb914fea9f7a9
vivianjohnson <59099112+vivianjohnson@users.noreply.github.com>
2020-05-21 16:51:10 -0700

    Prototype/particle effects and animations/in progress

--- JemRunner/Assets/Scripts/PlayerControllerJR.cs
+++ JemRunner/Assets/Scripts/PlayerControllerJR.cs
@@ -5,17 +5,21 @@ using UnityEngine;
 public class PlayerControllerJR : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private Animator playerAnim;
+    private int numLives = 3; 
     public float turnSpeed; 
     public float jumpForce = 10.0f;
     public float horizontalInput; 
     public float speed = 10.0f; 
     public bool isOnGround = true;
     public bool gameOver;
+    public ParticleSystem explosionParticle;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>(); 
     }
 
     // Update is called once per frame
@@ -31,8 +35,34 @@ public class PlayerControllerJR : MonoBehaviour
             {
                 playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
                 isOnGround = false;
+                playerAnim.SetTrigger("Jump_trig");
             }
-            isOnGround = true; 
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            numLives--;
+            Debug.Log("Oh no you have " + numLives + " lives left!");
+            if(numLives == 1)
+            {
+                Debug.Log("Oh no you have 1 life left! Be careful - get a powerup!");
+            }
+            if(numLives == 0)
+            {
+                Debug.Log("Game Over");
+                explosionParticle.Play();
+                playerAnim.SetBool("Death_b", true);
+                playerAnim.SetInteger("DeathType_int", 1);
+                gameOver = true;
+            }
+ 
+        } 
     }
 }
