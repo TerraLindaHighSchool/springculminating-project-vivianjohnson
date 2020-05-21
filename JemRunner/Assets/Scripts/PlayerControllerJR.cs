badc0225786afe51d28fb86811c49c8ebeb7c56b
vivianjohnson <59099112+vivianjohnson@users.noreply.github.com>
2020-05-20 17:33:11 -0700

    Prototype/Player movement/scene/ complete

--- JemRunner/Assets/Scripts/PlayerControllerJR.cs
+++ JemRunner/Assets/Scripts/PlayerControllerJR.cs
@@ -32,6 +32,7 @@ public class PlayerControllerJR : MonoBehaviour
                 playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
                 isOnGround = false;
             }
+            isOnGround = true; 
         }
     }
 }
