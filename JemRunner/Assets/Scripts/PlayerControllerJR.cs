334730a5bdf84ecaa370ced5a1c489ea94ebdffd
vivianjohnson <59099112+vivianjohnson@users.noreply.github.com>
2020-05-26 13:01:21 -0700

    Prototype/ particle effects/ complete

--- JemRunner/Assets/Scripts/PlayerControllerJR.cs
+++ JemRunner/Assets/Scripts/PlayerControllerJR.cs
@@ -6,7 +6,6 @@ public class PlayerControllerJR : MonoBehaviour
 {
     private Rigidbody playerRb;
     private Animator playerAnim;
-    private int numLives = 2; 
     public float turnSpeed; 
     public float jumpForce = 10.0f;
     public float horizontalInput; 
@@ -14,6 +13,7 @@ public class PlayerControllerJR : MonoBehaviour
     public bool isOnGround = true;
     public bool gameOver = false;
     public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle; 
 
     // Start is called before the first frame update
     void Start()
@@ -33,6 +33,7 @@ public class PlayerControllerJR : MonoBehaviour
             transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
             if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
             {
+                dirtParticle.Stop(); 
                 playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
                 isOnGround = false;
                 playerAnim.SetTrigger("Jump_trig");
@@ -42,21 +43,16 @@ public class PlayerControllerJR : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Ground"))
+        if (collision.gameObject.CompareTag("Road"))
         {
+            dirtParticle.Play(); 
             isOnGround = true;
         }
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
-           numLives--;
-            Debug.Log("Oh no you have " + numLives + " lives left!");
-            if(numLives == 1)
-            {
-                Debug.Log("Oh no you have 1 life left! Be careful - get a powerup!");
-            }
-            if(numLives == 0)
             {
                 explosionParticle.Play();
+                dirtParticle.Stop(); 
                 Debug.Log("Game Over");
                 playerAnim.SetBool("Death_b", true);
                 playerAnim.SetInteger("DeathType_int", 1);
