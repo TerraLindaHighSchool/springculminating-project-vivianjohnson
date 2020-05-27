9a9317aaee71e54b69bfae63f52162d74ba9f703
vivianjohnson <59099112+vivianjohnson@users.noreply.github.com>
2020-05-27 11:51:31 -0700

    Prototype/ending game & adding buttons/ in progress

--- JemRunner/Assets/Scripts/PlayerControllerJR.cs
+++ JemRunner/Assets/Scripts/PlayerControllerJR.cs
@@ -1,35 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro; 
 
 public class PlayerControllerJR : MonoBehaviour
 {
-    private Rigidbody playerRb;
     private Animator playerAnim;
-    public float turnSpeed; 
+    private Rigidbody playerRb;
+    private int gemCount = 0;
     public float jumpForce = 10.0f;
-    public float horizontalInput; 
-    public float speed = 10.0f; 
+    public float speed = 10.0f;
+    public float turnSpeed;
+    public float horizontalInput;
     public bool isOnGround = true;
     public bool gameOver = false;
+    public bool hasGem;
+    public bool isGameActive;
     public ParticleSystem explosionParticle;
     public ParticleSystem dirtParticle;
-    private int gemCount = 0;
-    public bool hasGem; 
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOvertext;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        isGameActive = true; 
         playerRb = GetComponent<Rigidbody>();
-        playerAnim = GetComponent<Animator>(); 
+        playerAnim = GetComponent<Animator>();
+        UpdateScore(0);
+        gameOvertext.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal"); 
+        horizontalInput = Input.GetAxis("Horizontal");
 
-        if(!gameOver)
+        while(isGameActive)
         {
             transform.Translate(Vector3.forward * Time.deltaTime * speed);
             transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
@@ -42,7 +50,7 @@ public class PlayerControllerJR : MonoBehaviour
             }
         }
     }
-
+ 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Road"))
@@ -62,24 +70,34 @@ public class PlayerControllerJR : MonoBehaviour
             }
  
         }
-        if(collision.gameObject.CompareTag("Finish Line"))
-        {
-            Debug.Log("You made it to the finish line! You beat the game! Now onto the next level");
-            dirtParticle.Stop();
-            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            playerAnim.SetTrigger("Jump_trig");
-            gameOver = true;
-             
-        }
     }
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Point"))
         {
-            gemCount++;
+            UpdateScore(1); 
             Debug.Log("Great job! You have: " + gemCount + " gems in your bag"); 
             hasGem = true;
             Destroy(other.gameObject); 
         }
+        if (other.CompareTag("Finish Line"))
+        {
+            Debug.Log("You made it to the finish line! You beat the game! Now onto the next level");
+            GameOver(); 
+
+        }
+    }
+
+    private void UpdateScore(int gemsToAdd)
+    {
+        gemCount += gemsToAdd;
+        scoreText.text = "Gems: " + gemCount; 
+    }
+
+    public void GameOver()
+    {
+        isGameActive = false; 
+        gameOvertext.gameObject.SetActive(true); 
     }
 }
