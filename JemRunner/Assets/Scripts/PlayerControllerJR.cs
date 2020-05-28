b5f0d35be93b2930f3f04004c6c8a3f635d66dd8
vivianjohnson <59099112+vivianjohnson@users.noreply.github.com>
2020-05-27 19:04:43 -0700

    Prototype/Complete

--- JemRunner/Assets/Scripts/PlayerControllerJR.cs
+++ JemRunner/Assets/Scripts/PlayerControllerJR.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro; 
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerControllerJR : MonoBehaviour
 {
     private Animator playerAnim;
+    public Button restartButton;
     private Rigidbody playerRb;
     private int gemCount = 0;
     public float jumpForce = 10.0f;
@@ -25,11 +28,12 @@ public class PlayerControllerJR : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        isGameActive = true; 
+        isGameActive = true;
         playerRb = GetComponent<Rigidbody>();
         playerAnim = GetComponent<Animator>();
         UpdateScore(0);
         gameOvertext.gameObject.SetActive(false);
+        restartButton.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -37,54 +41,55 @@ public class PlayerControllerJR : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal");
 
-        while(isGameActive)
+        if (isGameActive == true && !gameOver)
         {
             transform.Translate(Vector3.forward * Time.deltaTime * speed);
             transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
             if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
             {
-                dirtParticle.Stop(); 
+                dirtParticle.Stop();
                 playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
                 isOnGround = false;
                 playerAnim.SetTrigger("Jump_trig");
             }
         }
     }
- 
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Road"))
         {
-            dirtParticle.Play(); 
+            dirtParticle.Play();
             isOnGround = true;
         }
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
             {
                 explosionParticle.Play();
-                dirtParticle.Stop(); 
+                dirtParticle.Stop();
                 Debug.Log("Game Over");
                 playerAnim.SetBool("Death_b", true);
                 playerAnim.SetInteger("DeathType_int", 1);
                 gameOver = true;
+                GameOver(); 
             }
- 
+
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Point"))
+        if (other.CompareTag("Point"))
         {
-            UpdateScore(1); 
-            Debug.Log("Great job! You have: " + gemCount + " gems in your bag"); 
+            UpdateScore(1);
+            Debug.Log("Great job! You have: " + gemCount + " gems in your bag");
             hasGem = true;
-            Destroy(other.gameObject); 
+            Destroy(other.gameObject);
         }
         if (other.CompareTag("Finish Line"))
         {
             Debug.Log("You made it to the finish line! You beat the game! Now onto the next level");
-            GameOver(); 
+            GameOver();
 
         }
     }
@@ -92,12 +97,18 @@ public class PlayerControllerJR : MonoBehaviour
     private void UpdateScore(int gemsToAdd)
     {
         gemCount += gemsToAdd;
-        scoreText.text = "Gems: " + gemCount; 
+        scoreText.text = "Gems: " + gemCount;
     }
 
     public void GameOver()
     {
-        isGameActive = false; 
-        gameOvertext.gameObject.SetActive(true); 
+        isGameActive = false;
+        gameOvertext.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
